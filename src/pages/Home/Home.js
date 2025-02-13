35968318394554a2fc6ba0b17158475339265f7a
bhavinc8787 <bhavinc417@gmail.com>
2025-02-14 01:01:26 +0530

    Initial commit with existing projecte

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
-// import Section5 from "./Section5";
+import Section5 from "./Section5";
 import Section6 from "./Section6";
 import Section7 from "./Section7";
 import ContactMe from "./ContactUs.jsx";
@@ -21,13 +21,13 @@ const Home = () => {
         <Section2 />
 
         {/* Home Section Menu */}
-        {/* <Section3 /> */}
+        <Section3 />
 
         {/* Home Section Promotion */}
         <Section4 />
 
         {/* Home Section Shop */}
-        {/* <Section5 /> */}
+        <Section5 />
 
         {/* Home Section Blog */}
         <Section6 />
